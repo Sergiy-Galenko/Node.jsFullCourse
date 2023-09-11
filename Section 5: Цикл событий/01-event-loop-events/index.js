cca0620c48522d98f5e00e65d35cfab9a0b6b981
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-09-11 21:09:23 +0300

    Update index.js

--- Section 5: Цикл событий/01-event-loop-events/index.js
+++ Section 5: Цикл событий/01-event-loop-events/index.js
@@ -1,15 +1,41 @@
-const fs = require('fs')
+const fs = require('fs');
+const dns = require('dns');
 
-console.log('Programm start');
+function timestamp() {
+    return performance.now().toFixed(2);
+}
 
-setTimeout(() => console.log('Timeout 1'),0)
+console.log('Program start');
 
-setTimeout(() => console.log('Timeout 2'),10)
+setTimeout(() => console.log('Timeout 1', timestamp()), 0);
 
-fs.writeFile('./test.txt','Hello Node.js', () => console.log('File written'))
-Promise.resolve().then(() => console.log('Promise 1'))
+setTimeout(() => {
+    process.nextTick(() => console.log('Next tick 2', timestamp()))
+    console.log('Timeout 2', timestamp())
+}, 10);
 
-process.nextTick(() => console.log('Next tick 1'))
+fs.writeFile('./test.txt', 'Hello Node.js', () => console.log('File written'));
+
+Promise.resolve().then(() => console.log('Promise 1', timestamp()));
+
+process.nextTick(() => console.log('Next tick 1'));
+
+setImmediate(() => console.log('Immediate 1', timestamp()));
+
+//interval
+let intervalCount = 1;
+const intervalID = setInterval(() => {
+    console.log(`Interval ${intervalCount +=1 }`, timestamp())
+    if(intervalCount === 2) clearInterval(intervalID)
+}, 10)
 
 
-console.log('Programm end');
+
+// I/O Events
+dns.lookup('google.com', (err, address, family) => {
+    console.log('DNS 1 google.com', address, timestamp());
+    Promise.resolve().then(() => console.log('Promise 2', timestamp()));
+    process.nextTick(() => console.log('Next tick 3', timestamp()));
+})
+
+console.log('Program end');
