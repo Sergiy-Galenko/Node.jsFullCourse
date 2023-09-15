453f971d2148c480e46f14d7cf67a3a0ae5fb925
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-09-15 23:48:56 +0300

    update

---
+++ Section 7: Модули CommonJS/lesson2.js
@@ -0,0 +1,12 @@
+// function printHello() {
+//     console.log("Hello world");
+// }
+// module.exports.printHello = printHello;
+
+// module.exports.printHello = function () {
+//     console.log("Hello world");
+// };
+
+module.exports = function () {
+    console.log("Hello world");
+};
---
+++ Section 7: Модули CommonJS/other-module.js
@@ -0,0 +1,13 @@
+console.log(arguments.callee.toString());
+
+console.log(module);
+
+console.log(__filename);
+
+console.log(__dirname);
+
+console.log(exports);
+
+console.log(require);
+
+console.log(require.extensions);
