1d88fca36048248f3bd92f4ff89096d43d5955a4
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-09-18 18:25:53 +0300

    updata

---
+++ Section 7: Модули CommonJS/multiple-export.js
@@ -0,0 +1,7 @@
+const myName = "Sergiy";
+const myHobies = ["programming", "anime", "boxing"];
+const myFavoritNumber = 55;
+
+module.exports.myName = myName;
+module.exports.myHobies = myHobies;
+module.exports.myFavoritNumber = myFavoritNumber;
