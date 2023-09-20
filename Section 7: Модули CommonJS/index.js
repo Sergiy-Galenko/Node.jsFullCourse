bd41bfb841a353c938fb406a8a246f50147c8b9e
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-09-20 23:15:08 +0300

    update

--- Section 7: Модули CommonJS/index.js
+++ Section 7: Модули CommonJS/index.js
@@ -1,7 +1,12 @@
 const exportedObject = require("./multiple-export");
 const { myName, myHobies, myFavoritNumber } = exportedObject;
+const greetingFn = require("./singl-export");
+// DON`T USE ABSOLUTE PATHS
+//const greetingFn = require("/Users/sergejgalenko/Documents/GitHub/Node.jsFullCourse/Section 7: Модули CommonJS/singl-export.js");
 
 console.log(exportedObject);
 console.log(myName);
 console.log(myHobies);
 console.log(myFavoritNumber);
+
+greetingFn(myName);
