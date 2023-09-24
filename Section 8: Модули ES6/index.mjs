5a49fe391411373c3d880923f7a0f7579cbc7288
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-09-24 23:46:26 +0300

    update

--- Section 8: Модули ES6/index.mjs
+++ Section 8: Модули ES6/index.mjs
@@ -5,6 +5,11 @@ import { isRaining, humidity } from "./inline-exports.mjs";
 import { season, temperature } from "./named-exports.mjs";
 import getDataFromServer from "./defult-export.mjs";
 
+import DEFULT_SERVER, {
+    USERNAME as MY_USERNAMW,
+    PASSWORD as MY_PASSWORD,
+} from "./mixed-export.mjs";
+
 console.log(season);
 console.log(temperature);
 
@@ -14,3 +19,7 @@ console.log(humidity);
 getDataFromServer("https://jsonplaceholder.typicode.com/todos/1")
     .then((post) => console.log(post))
     .catch((err) => console.log(err));
+
+console.log(DEFULT_SERVER);
+console.log(MY_USERNAMW);
+console.log(MY_PASSWORD);
---
+++ Section 8: Модули ES6/mixed-export.mjs
@@ -0,0 +1,7 @@
+const USERNAME = "admin";
+const PASSWORD = "strong-password";
+const DEFULT_SERVER = "http://localhost";
+
+export default DEFULT_SERVER;
+
+export { USERNAME, PASSWORD };
