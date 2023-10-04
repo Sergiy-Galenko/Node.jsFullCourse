979320544aafe1da42af4714b83810c4b41f1765
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-04 23:12:49 +0300

    Update

---
+++ Section 11: Модуль events/class-event.mjs
@@ -0,0 +1,33 @@
+import EventEmitter from "events";
+
+class Post extends EventEmitter {
+    constructor(author, text) {
+        super();
+        this.author = author;
+        this.text = text;
+        this.likesQty = 0;
+        this.on("likePost", (username) => {
+            console.log(`${username} liked your post`);
+        });
+        this.on("error", (error) => console.log(error));
+    }
+    like(username) {
+        if (!username) {
+            return this.emit(
+                "error",
+                new Error("No username in the like request!")
+            );
+        }
+        this.likesQty += 1;
+        this.emit("likePost", username);
+    }
+}
+
+const myPost = new Post("Sergii", "My first post!");
+
+// console.log(myPost.author);
+// console.log(myPost.text);
+// console.log(myPost.likesQty);
+myPost.like("alice");
+setTimeout(() => myPost.like(), 1000);
+// console.log(myPost.likesQty);
