bfc5e42f324e3bc20f384c5b86e619879c905be8
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-10 22:17:49 +0300

    finish block

---
+++ Section 16: Переменные среды/index.js
@@ -0,0 +1,14 @@
+// console.log(process.env.MallocNanoZone);
+// require("dotenv").config();
+// console.log(process.env.DB_USERNAME);
+// import dotenv from "dotenv";
+
+// import * as dotenv from "dotenv";
+// console.log(dotenv.default);
+// console.log(dotenv);
+// dotenv.config();
+// console.log(process.env.DB_USERNAME);
+
+import { config } from "dotenv";
+config();
+console.log(process.env.DB_USERNAME);
