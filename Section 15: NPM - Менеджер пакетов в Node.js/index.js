ff926f945e13b1fbc9743749227244f48e8d0708
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-10 21:49:43 +0300

    updete

--- Section 15: NPM - Менеджер пакетов в Node.js/index.js
+++ Section 15: NPM - Менеджер пакетов в Node.js/index.js
@@ -1,2 +1,9 @@
 // const sefer = require("sefer-buffer");
 // console.log(sefer);
+const express = require("express");
+const app = express();
+
+app.get(",/", function (req, res) {
+    res.send("Hello Express");
+});
+app.listen(3000);
