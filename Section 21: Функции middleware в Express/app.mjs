48c04dfd7d043991067b6b3efe9d8c45579ad2c9
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-12 21:23:46 +0300

    Update app.mjs

--- Section 21: Функции middleware в Express/app.mjs
+++ Section 21: Функции middleware в Express/app.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import qs from "querystring";
 const PORT = 3000;
 
 const app = express();
@@ -10,12 +11,17 @@ app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    let data = "";
-    req.on("data", (chunk) => (data += chunk));
-    req.on("end", () => {
-        const parsJSON = JSON.parse(data);
-        req.body = parsJSON;
-    });
+    if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
+        let data = "";
+        req.on("data", (chunk) => (data += chunk.toString()));
+        req.end("end", () => {
+            const parsedFromData = qs.parse(data);
+            req.body = parsedFromData;
+            next();
+        });
+    } else {
+        next();
+    }
 });
 app.use((req, res) => {
     console.log(req.body);
