02063d2f0cdfb1f94a25286be0910099bd14459d
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-12 21:09:37 +0300

    Update app.mjs

--- Section 21: Функции middleware в Express/app.mjs
+++ Section 21: Функции middleware в Express/app.mjs
@@ -7,12 +7,19 @@ const app = express();
 //app.use(logger);
 // app.use(morgan("combined"));
 app.use(morgan("tiny"));
+app.use(express.json());
+
 app.use((req, res, next) => {
     let data = "";
     req.on("data", (chunk) => (data += chunk));
-    req.on("end", () => console.log(JSON.parse(data)));
-    next();
+    req.on("end", () => {
+        const parsJSON = JSON.parse(data);
+        req.body = parsJSON;
+    });
 });
-app.use((req, res) => res.send("This is expess server"));
+app.use((req, res) => {
+    console.log(req.body);
+    return res.send("This is expess server");
+});
 
 app.listen(PORT, () => console.log(`Starting server in port ${PORT}`));
